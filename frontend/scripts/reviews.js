5156c65a45338595dd3bb97140e54412dc874d32
leviives28 <149870357+leviives28@users.noreply.github.com>
2023-11-06 07:06:23 +0000

    Update reviews.js
    Added in a conditional to check if the current value of a review is null, and remove it from the list of reviews if so.

--- frontend/scripts/reviews.js
+++ frontend/scripts/reviews.js
@@ -8,6 +8,11 @@ if (params.has('review_id')) {
 async function get_reviews() {
     const response = await fetch('https://leviives.dev/reviews/get_reviews');
     reviews = await response.json();
+    for (let rev in reviews) {
+        if (!reviews[rev].review) {
+            reviews.splice(rev, 1);
+        }
+    }
 }
 
 const escapeHTML = str =>
@@ -25,6 +30,7 @@ const escapeHTML = str =>
 
 get_reviews()
 .then(() => {
+    console.log(reviews);
     let iterator = 1;
     document.getElementById('currentReviewMessage').innerHTML = escapeHTML(reviews[0].review);
     document.getElementById('projectLink').innerHTML = !reviews[0].website ? `<p id="nolink">Closed source project</p>` : `<a href=${reviews[0].website} target="_blank" class="link">${reviews[0].website}</a>`;
