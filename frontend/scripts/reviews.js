ae769ce06f25eb391a83d5374b51081ce67bf95e
leviives28 <149870357+leviives28@users.noreply.github.com>
2023-11-09 12:21:23 +0000

    Update reviews.js
    Altered review script so that the newest reviews are shown first.
    Had to do this in javascript and not as part of the sql query as no date column in reviews database table.

--- frontend/scripts/reviews.js
+++ frontend/scripts/reviews.js
@@ -30,6 +30,7 @@ const escapeHTML = str =>
 
 get_reviews()
 .then(() => {
+    reviews.reverse();
     let iterator = 1;
     document.getElementById('currentReviewMessage').innerHTML = escapeHTML(reviews[0].review);
     document.getElementById('projectLink').innerHTML = !reviews[0].website ? `<p id="nolink">Closed source project</p>` : `<a href=${reviews[0].website} target="_blank" class="link">${reviews[0].website}</a>`;
